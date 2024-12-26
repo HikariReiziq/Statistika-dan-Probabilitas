8f3c46eb23bb4ae01d91f9c5ff084e1b067875ad
HikariReiziq <reiziqzip@gmail.com>
2024-12-27 06:01:38 +0700

    Operasi aritmatika

---
+++ 8_OperasiAritmatika.py
@@ -0,0 +1,37 @@
+#Operasi Aritmatika dlm Python
+
+#jumlah (+)
+a = 10
+b = 5
+penjumlahan = a + b
+print("Pjumlahn:", penjumlahan)#15
+
+#Pengurangan (-)
+pengurangan = a - b
+print("Pengurangan:", pengurangan)#5
+
+#Perkalian (*)
+perkalian = a * b
+print("Perkalian:", perkalian)  #50
+
+#Pembagian (/)
+pembagian = a / b  #Hasilnya berupa float
+print("Pembagian:", pembagian)  # Output: 2.0
+
+#Pembagian Bulat (//)
+hasil_pembagian_bulat = a // b  #Hasil berupa integer
+print("Pembagian Bulat:", hasil_pembagian_bulat)#Output: 2
+
+#Modulus (%)
+hasil_modulus = a % b# Sisa pembagian
+print("Modulus:", hasil_modulus)# Output: 0
+
+#Perpangkatan (**)
+hasil_pangkat = a ** b# a pangkat b (10^5)
+print("Perpangkatan:", hasil_pangkat)#utput: 100000
+
+#Operator Campuran
+c = 7
+d = 3
+hasil_campuran = (c + d) * a / b - c
+print("Hasil Operasi Campuran:", hasil_campuran) #13
